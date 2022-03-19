c6d002c270bc5d1207fe35c8643174c3b3aba9e7
Dmitriy Karavanov <karavanovd@gmail.com>
2022-03-20 00:55:34 +1000

    Customer model refactored

--- models/Customer.php
+++ models/Customer.php
@@ -13,16 +13,6 @@ use yii\db\ActiveRecord;
  */
 class Customer extends ActiveRecord
 {
-    private const QUALITY_ACTIVE = 'active';
-    private const QUALITY_REJECTED = 'rejected';
-    private const QUALITY_COMMUNITY = 'community';
-    private const QUALITY_UNASSIGNED = 'unassigned';
-    private const QUALITY_TRICKLE = 'trickle';
-
-    private const TYPE_LEAD = 'lead';
-    private const TYPE_DEAL = 'deal';
-    private const TYPE_LOAN = 'loan';
-
     /**
      * @inheritdoc
      */
@@ -49,59 +39,5 @@ class Customer extends ActiveRecord
         return [
             'name' => Yii::t('app', 'Name'),
         ];
-    }
-
-    /**
-     * Returns associative array of readable customer qualities. Key of array is quality code
-     *
-     * @return array
-     */
-    public static function getQualityTexts(): array
-    {
-        return [
-            self::QUALITY_ACTIVE => Yii::t('app', 'Active'),
-            self::QUALITY_REJECTED => Yii::t('app', 'Rejected'),
-            self::QUALITY_COMMUNITY => Yii::t('app', 'Community'),
-            self::QUALITY_UNASSIGNED => Yii::t('app', 'Unassigned'),
-            self::QUALITY_TRICKLE => Yii::t('app', 'Trickle'),
-        ];
-    }
-
-    /**
-     * Returns readable quality text by quality code
-     *
-     * @param string|null $quality
-     *
-     * @return string|null
-     */
-    public static function getQualityTextByQuality(?string $quality): ?string
-    {
-        return self::getQualityTexts()[$quality] ?? $quality;
-    }
-
-    /**
-     * Returns associative array of readable customer types. Key of array is type code
-     *
-     * @return array
-     */
-    public static function getTypeTexts(): array
-    {
-        return [
-            self::TYPE_LEAD => Yii::t('app', 'Lead'),
-            self::TYPE_DEAL => Yii::t('app', 'Deal'),
-            self::TYPE_LOAN => Yii::t('app', 'Loan'),
-        ];
-    }
-
-    /**
-     * Returns readable type text by type code
-     *
-     * @param string $type
-     *
-     * @return mixed
-     */
-    public static function getTypeTextByType(string $type): string
-    {
-        return self::getTypeTexts()[$type] ?? $type;
     }
 }
